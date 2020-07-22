363056acc24bd12ebd9e9544da97be3fde89ea55
hiddensbrain <hiddensmaster@gmail.com>
2020-07-22 13:09:35 +0700

    uploaded first version

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.courtcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int counterA = 0;
+    int counterB = 0;
+
+
+    public void count3A (View view) {
+        counterA = counterA + 3;
+        displayForTeamA(counterA);
+    }
+
+    public void count2A (View view) {
+        counterA = counterA + 2;
+        displayForTeamA(counterA);
+    }
+
+    public void count1A (View view) {
+        counterA++;
+        displayForTeamA(counterA);
+    }
+
+    public void count3B (View view) {
+        counterB = counterB + 3;
+        displayForTeamB(counterB);
+    }
+
+    public void count2B (View view) {
+        counterB = counterB + 2;
+        displayForTeamB(counterB);
+    }
+
+    public void count1B (View view) {
+        counterB++;
+        displayForTeamB(counterB);
+    }
+
+    public void reset (View view) {
+        counterB = 0;
+        counterA = 0;
+        displayForTeamA(counterA);
+        displayForTeamB(counterB);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
