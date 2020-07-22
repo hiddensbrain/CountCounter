1f8f721a6c8da5f766571dff7e929651176ce11d
hiddensbrain <hiddensmaster@gmail.com>
2020-07-22 13:36:06 +0700

    main changes

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -11,37 +11,37 @@ public class MainActivity extends AppCompatActivity {
     int counterB = 0;
 
 
-    public void count3A (View view) {
+    public void count3A(View view) {
         counterA = counterA + 3;
         displayForTeamA(counterA);
     }
 
-    public void count2A (View view) {
+    public void count2A(View view) {
         counterA = counterA + 2;
         displayForTeamA(counterA);
     }
 
-    public void count1A (View view) {
+    public void count1A(View view) {
         counterA++;
         displayForTeamA(counterA);
     }
 
-    public void count3B (View view) {
+    public void count3B(View view) {
         counterB = counterB + 3;
         displayForTeamB(counterB);
     }
 
-    public void count2B (View view) {
+    public void count2B(View view) {
         counterB = counterB + 2;
         displayForTeamB(counterB);
     }
 
-    public void count1B (View view) {
+    public void count1B(View view) {
         counterB++;
         displayForTeamB(counterB);
     }
 
-    public void reset (View view) {
+    public void reset(View view) {
         counterB = 0;
         counterA = 0;
         displayForTeamA(counterA);
@@ -54,10 +54,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    /**
+     * Displays the given score for Team A.
+     */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+
+    /**
+     * Displays the given score for Team B.
+     */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
